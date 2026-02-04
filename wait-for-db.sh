768d93ee9305517ffa6103f4f0fef67936d825ce
SnapXv1 <snapx.betav1@gmail.com>
2026-02-04 15:46:34 +0100

    Update wait-for-db.sh - hardcore port & host

--- wait-for-db.sh
+++ wait-for-db.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 echo "Waiting for database to be ready..."
 
-until nc -z "$DB_HOST" "$DB_PORT"; do
+until nc -z "dpg-d61ioschg0os73d145ig-a" "5432"; do
   echo "Database not reachable yet..."
   sleep 2
 done
 
 echo "Database is reachable. Starting n8n..."
 n8n start
+
